c21d5f0c591388da52007b476a799c38c6fadabd
SaschaMoke <lennysieber@icloud.com>
2025-01-09 15:03:59 +0100

    TestCommit

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
             NavigationDestination(
               selectedIcon: Icon(Icons.route),
               icon: Icon(Icons.route_outlined),
-              label: 'Verkehr',
+              label: 'Sexualverkehr',
             ),
                  /*      NavigationDestination(
               icon: Badge(
