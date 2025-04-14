5277aa713d8fcd349b29798628f86e4d416ad477
SaschaMoke <lennysieber@icloud.com>
2025-04-14 19:43:35 +0200

    WEITER GEHTS OMG bin glaube ich gerade fett am kochen

---
+++ lib/Gewerbe/Gewerbe_Module/oeffnung.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Oeffnungszeiten extends StatefulWidget {
+  final String monday;
+  final String tuesday;
+  final String wednesday;
+  final String thursday;
+  final String friday;
+  final String saturday;
+  final String sunday;
+  final BoxConstraints constraints;
+  final String leadingHint;
+  final String trailingHint;
+  final bool leadingImportant;
+  final bool trailingImportant;
+  //... weiteres
+  
+  const Oeffnungszeiten({
+    super.key,
+    required this.monday,
+    required this.tuesday,
+    required this.wednesday,
+    required this.thursday,
+    required this.friday,
+    required this.saturday,
+    required this.sunday,
+    required this.constraints,
+    this.leadingHint = '',
+    this.trailingHint = '',
+    this.leadingImportant = false,
+    this.trailingImportant = false,
+  });
+  @override
+  State<Oeffnungszeiten> createState() => _OeffnungszeitenState();
+}
+
+class _OeffnungszeitenState extends State<Oeffnungszeiten> {
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      leading: Icon(Icons.timelapse_outlined), 
+      title: Text(
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: widget.constraints.maxWidth*0.075,
+        ),
+        'Öffnungszeiten'
+      ),
+      shape: const Border(),
+      tilePadding: EdgeInsets.all(1),
+      childrenPadding: EdgeInsets.all(5),
+      textColor: Color.fromARGB(255, 50, 150, 50),
+      iconColor: Color.fromARGB(255, 50, 150, 50),   //hallooooooo
+      children: [
+        widget.leadingHint.isNotEmpty ? Text(
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: widget.constraints.maxWidth*0.04,
+            color: widget.leadingImportant ? Color.fromARGB(255, 255, 0, 0):Color.fromARGB(255, 0, 0, 0),
+          ),
+          '''${widget.leadingHint}
+'''
+        ):SizedBox(),
+        Text(
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: widget.constraints.maxWidth*0.04
+          ),
+          '''Montag: ${widget.monday}
+Dienstag: ${widget.tuesday}
+Mittwoch: ${widget.wednesday}
+Donnerstag: ${widget.thursday}
+Freitag: ${widget.friday}
+Samstag: ${widget.saturday}
+Sonntag: ${widget.sunday}'''
+        ),
+        widget.trailingHint.isNotEmpty ? Text(
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: widget.constraints.maxWidth*0.04,
+            color: widget.trailingImportant ? Color.fromARGB(255, 255, 0, 0):Color.fromARGB(255, 0, 0, 0),
+          ),
+          '''
+
+${widget.trailingHint}'''
+        ):SizedBox(),
+      ],
+    );
+  }
+}
---
+++ lib/Gewerbe/gewerbeseite.dart
@@ -0,0 +1,113 @@
+import 'package:eichwalde_app/Gewerbe/Gewerbe_Module/kontakt.dart';
+import 'package:eichwalde_app/Gewerbe/Gewerbe_Module/social.dart';
+import 'package:flutter/material.dart';
+import 'Gewerbe_Module/oeffnung.dart';
+
+class Gewerbeseite extends StatefulWidget{
+  const Gewerbeseite({super.key});
+
+  @override
+  State<Gewerbeseite> createState() => _GewerbeseiteState();
+}
+//fontSize auf anderen Geräten
+class _GewerbeseiteState extends State<Gewerbeseite> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hi'),
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.favorite_outline)
+          )
+        ],
+      ),
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width*0.9,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return ListView(
+                children: [
+                  SizedBox(height: 20),
+                  Image.network(
+                    'https://i.ytimg.com/vi/yzCtDA6tHLo/maxresdefault.jpg'
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: constraints.maxWidth*0.1
+                    ),
+                    'Bertram GmbH'
+                  ),
+                  SizedBox(height: 20),
+                  ExpansionTile(
+                    leading: Icon(Icons.description_outlined), //evtl.
+                    title: Text(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: constraints.maxWidth*0.075,
+                      ),
+                      'Beschreibung'
+                    ),
+                    shape: const Border(),
+                    tilePadding: EdgeInsets.all(1),
+                    childrenPadding: EdgeInsets.all(5),
+                    textColor: Color.fromARGB(255, 50, 150, 50),
+                    iconColor: Color.fromARGB(255, 50, 150, 50), 
+                    children: [
+                      Text(
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: constraints.maxWidth*0.04
+                        ),
+                        'Lorem ipsum salami hallo ich bin ein langer Text um das mal ein bisschen zu füllen Bom schalom Schames lalala Moke bom'
+                      )
+                    ],
+                  ),
+
+                  //weitere Module hier drunter:
+                  Oeffnungszeiten(
+                    monday: '08:00 - 16:00 Uhr', 
+                    tuesday: '08:00 - 16:00 Uhr', 
+                    wednesday: '08:00 - 16:00 Uhr', 
+                    thursday: '08:00 - 16:00 Uhr', 
+                    friday: '08:00 - 16:00 Uhr', 
+                    saturday: 'Geschlossen', 
+                    sunday: 'Geschlossen', 
+                    constraints: constraints,
+                    leadingHint: 'Abweichende Öffnungszeiten durch Osterferien',
+                    leadingImportant: true,
+                    trailingHint: 'Bottom text',
+                    trailingImportant: true,
+                  ),
+                  SocialMedia(
+                    instagramName: 'Bertram0815',
+                    facebookName: 'Bertram0815',
+                    showFacebook: true,
+                    showInstagram: true,
+                    constraints: constraints
+                  ),
+                  Kontakt(
+                    adresse: 'Bertramstraße 69',
+                    web: 'bertram-website.de',
+                    telefon: '+69 1234 56789',
+                    mail: 'bertram@gmail.com',
+                    constraints: constraints
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+    
+    //return SafeArea(
+      
+      //child: child
+    //);
+  }
+}
